e06a64f696eaf7d3e64a4f0499e6035a45d85d27
Jonathan Masukawa <jonathan.masukawa@gmail.com>
2016-05-17 12:08:05 +0000

    Updated checkpoint files and fixed some typos

---
+++ checkpoint/checkpoint-2-sign.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Thanks</title>
+</head>
+<body>
+    <h1>Thanks for signing my guestbook!</h1>
+    <?php
+    // Only do anything if a post request was sent to this form handler.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the visitor's information by the "name" attribute of the input
+        // element in the user HTML form.
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $color = $_POST['color'];
+        
+        // Write the information back to the document for testing purposes.
+        // We'll remove this later.
+        echo "<p>You entered:";
+        echo "<br>First name: $firstname";
+        echo "<br>Last name:$lastname";
+        echo "<br>Favorite Color: $color";
+        echo "</p>";
+    }
+    ?>
+</body>
+</html>
---
+++ checkpoint/checkpoint-2-view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Viewing Guestbook</title>
+</head>
+<body>
+    <h1>Guestbook</h1>
+    <p>These people signed my guestbook:</p>
+    <?php
+        echo "<p>Jon</p>"; // replace my name with your name
+    ?>
+</body>
+</html>
